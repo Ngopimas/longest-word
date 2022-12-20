f6e6f2d4dec9f0c28cb1b67fe95a6e2144965998
Romain Coupey <schaft115@hotmail.com>
2022-12-20 11:28:16 +0000

    update typo

--- tests/test_game.py
+++ tests/test_game.py
@@ -45,7 +45,7 @@ class TestGame:
         assert new_game.grid == list(test_grid)  # Make sure the grid remained untouched
 
     def test_unknown_word_is_invalid(self):
-        """A word that is not in the English dictionary should no be valid"""
+        """A word that is not in the English dictionary should not be valid"""
         new_game = Game()
         new_game.grid = list("KWIENFUQW")  # Force the grid to a test case:
         assert new_game.is_valid("FEUN") is False
