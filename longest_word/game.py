eb1311732e9b80e39fec28941c33db2860208d56
Romain Coupey <schaft115@hotmail.com>
2022-12-20 11:01:29 +0000

    Game development with TDD

---
+++ longest_word/game.py
@@ -0,0 +1,23 @@
+# pylint: disable=missing-docstring
+# pylint: disable=too-few-public-methods
+
+import string
+import random
+
+
+class Game:
+    def __init__(self) -> list:
+        """Attribute a random grid to size 9"""
+        self.grid = random.choices(string.ascii_uppercase, k=9)
+
+    def is_valid(self, word: str) -> bool:
+        """Return True if and only if the word is valid, given the Game's grid"""
+        if not word:
+            return False
+        letters = self.grid.copy()  # Consume letters from the grid
+        for letter in word:
+            if letter in letters:
+                letters.remove(letter)
+            else:
+                return False
+        return True
